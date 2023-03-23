a77ef8132f7db597a2689961c8cf5112095792a3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-03-24 00:52:02 +0100

    minor refactor

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
 mod notes;
 
 fn main() {
-    let quizing = true;
-
     let fave_key = notes::AltNote {
         name: notes::NoteName::D,
         alteration: notes::Alteration::FLAT,
     };
-    println!("My favorite key is {}\n", fave_key.repr());
-    println!("It's {} semitones from C", fave_key.distance_from_c());
+    println!("My favorite key is {}", fave_key.repr());
+    println!("It's {} semitones from C\n", fave_key.distance_from_c());
 
+    let quizing = true;
     if quizing {
-        let random_note = notes::Note::get_random();
-        println!("Whate note is a fifth above {}?", random_note.to_string());
-        let user_note = notes::Note::get_from_user();
-        match user_note.is_a_fifth_above(&random_note) {
-            true => println!("ding ding you win"),
-            false => println!("[EXTREMELY LOUD INCORRECT BUZZER]"),
-        }
+        quiz();
+    }
+}
+
+fn quiz() {
+    let random_note = notes::Note::get_random();
+    println!("Whate note is a fifth above {}?", random_note.to_string());
+    let user_note = notes::Note::get_from_user();
+    match user_note.is_a_fifth_above(&random_note) {
+        true => println!("ding ding you win"),
+        false => println!("[EXTREMELY LOUD INCORRECT BUZZER]"),
     }
 }
