71ca27c38d052bdce2d25ce021834f1f80574ffe
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-03-24 02:59:36 +0100

    generate lilypond file automatically

--- assets/sheet_music_maker/C4.ly
+++
@@ -1,9 +0,0 @@
-\version "2.22.2"
-
-
-\relative {
-\time 4/4
-\clef treble
-\key c \major
-| c'1 |
-}
--- assets/sheet_music_maker/make_note.sh
+++ assets/sheet_music_maker/make_note.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env sh
 
-MAIN=C4
+MAIN=main
+
+cat > "${MAIN}.ly" <<- EOM
+\version "2.22.2"
+
+\relative {
+\time 4/4
+\clef treble
+\key c \major
+| c'1 |
+}
+EOM
 
 lilypond "${MAIN}.ly"
 open "${MAIN}.pdf"
