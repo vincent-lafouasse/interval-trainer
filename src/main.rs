fc2e644587cf5d7d0df5728dc478f62c91fb1e59
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-03-25 17:16:32 +0100

    minor refactor

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,8 @@ fn quiz() {
     let random_note = Note::get_random();
     println!("Whate note is \na perfect fifth above {}?", random_note);
     let user_note = Note::get_from_user();
-    match (user_note.distance_from_c() - random_note.distance_from_c()).rem_euclid(CHROMATIC_NOTES_PER_OCTAVE)
+    match (user_note.distance_from_c() - random_note.distance_from_c())
+        .rem_euclid(CHROMATIC_NOTES_PER_OCTAVE)
         == 7
     {
         true => println!("ding ding you win"),
