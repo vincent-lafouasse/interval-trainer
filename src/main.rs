548aa922134303650478a36424f8f188186a6247
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-03-25 16:44:37 +0100

    remove useless debug prints

--- src/main.rs
+++ src/main.rs
@@ -22,37 +22,12 @@ fn main() -> Result<()> {
 }
 
 fn debug() {
-    let note_name = NoteName::A;
-    println!(
-        "{}{}{}{}{}{}{}{}",
-        note_name,
-        note_name.next(),
-        note_name.next().next(),
-        note_name.next().next().next(),
-        note_name.next().next().next().next(),
-        note_name.next().next().next().next().next(),
-        note_name.next().next().next().next().next().next(),
-        note_name.next().next().next().next().next().next().next(),
-    );
-
-    let fave_key = Note {
-        name: NoteName::D,
-        alteration: Alteration::Flat,
-    };
-    println!("My favorite key is {}", fave_key);
-    println!("It's {} semitones from C\n", fave_key.distance_from_c());
-
-    println!("Cool new way to print Notes: {}", fave_key);
-
     let other_key = Note::parse_from_string("Eb").expect("Huh oh that's not a good note");
     println!(
         "Another good one is {}, which is {} semitones from C\n",
         other_key,
         other_key.distance_from_c()
     );
-
-    let random_note = Note::get_random();
-    println!("Here's a random note: {}\n", random_note);
 
     let an_interval = Interval {
         base_interval: BaseInterval::Third,
