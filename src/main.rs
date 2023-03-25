945e2a0c0f38caeb0454cdd207e53994e1b3ef18
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-03-25 17:16:03 +0100

    minor refactor

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use color_eyre::eyre::Result;
 
 use crate::intervals::{BaseInterval, Interval, Quality};
 #[allow(unused_imports)]
-use crate::notes::{Alteration, Note, NoteName, NOTES_PER_OCTAVE};
+use crate::notes::{Alteration, Note, NoteName, CHROMATIC_NOTES_PER_OCTAVE};
 
 fn main() -> Result<()> {
     color_eyre::install()?;
@@ -43,7 +43,7 @@ fn quiz() {
     let random_note = Note::get_random();
     println!("Whate note is \na perfect fifth above {}?", random_note);
     let user_note = Note::get_from_user();
-    match (user_note.distance_from_c() - random_note.distance_from_c()).rem_euclid(NOTES_PER_OCTAVE)
+    match (user_note.distance_from_c() - random_note.distance_from_c()).rem_euclid(CHROMATIC_NOTES_PER_OCTAVE)
         == 7
     {
         true => println!("ding ding you win"),
