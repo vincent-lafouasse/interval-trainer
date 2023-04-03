c991ccff559aac915cf50fd945eb277316b76517
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-04-03 16:33:24 +0200

    minor refactor

--- assets/sheet_music_maker/sheet_music_maker.sh
+++ assets/sheet_music_maker/sheet_music_maker.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env sh
 
+set -o xtrace
+
 #################################################################################
 
 MAIN="main"
-AUX_DIR="./aux"
-TARGET_DIR="./target"
+AUX_DIR="aux"
+TARGET_DIR="target"
 mkdir "${AUX_DIR}"
 mkdir "${TARGET_DIR}"
 
 TEMPLATE="template/template.ly"
 LILY_FILE="${AUX_DIR}/${MAIN}.ly"
-OUTPUT="${TARGET_DIR}/${MAIN}.ly"
+OUTPUT="${TARGET_DIR}/${MAIN}.pdf"
 
 TIME_SIG="2/4"
 CLEF="treble"
 NOTES="c'2"
 export TIME_SIG CLEF NOTES
 
-cat "${TEMPLATE}" | envsubst > "${LILY_FILE}"
-lilypond "${LILY_FILE}" --output="${TARGET_DIR}"
+envsubst < "${TEMPLATE}" > "${LILY_FILE}"
+lilypond --output="${TARGET_DIR}" "${LILY_FILE}"
 open "${OUTPUT}"
 
 rm -rf "${AUX_DIR}"
