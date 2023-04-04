8408c8adca447e64d95ee06b3d3b2c6fe1298cfd
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-04-04 21:57:05 +0200

    comment

--- src/synth.rs
+++ src/synth.rs
@@ -1,6 +1,7 @@
 use core::time::Duration;
 use rodio::source::Source;
 
+/// A wavetable oscillator that can play sound via the `rodio::source::Source` trait
 #[derive(Copy, Clone)]
 pub struct Oscillator {
     sample_rate: usize,
@@ -15,11 +16,17 @@ impl Oscillator {
     }
 
     pub fn set_frequency(&mut self, frequency: f32) {
+        // how much to move in the wavetable per tick
+        // linear in self.wavetable.resolution()
+        // linear in frequency (higher f => bigger increment to get more periods per units of time)
+        // inverse in sample_rate because "per tick"
         self.index_increment =
-            frequency * (self.wavetable.resolution() as f32) / (self.sample_rate as f32);
+            (self.wavetable.resolution() as f32) * frequency / (self.sample_rate as f32);
     }
 
     pub fn get_sample(&mut self) -> f32 {
+        // the wavetable is discrete so non-integer values must be estimated
+        // here by linear interpolation
         let sample = self.wavetable.interpolate(self.index);
         self.index += self.index_increment;
         self.index %= self.wavetable.resolution() as f32;
@@ -27,26 +34,33 @@ impl Oscillator {
     }
 }
 
-impl Iterator for Oscillator {
-    type Item = f32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        Some(self.get_sample())
-    }
-}
-
 impl Source for Oscillator {
     fn channels(&self) -> u16 {
+        // number of channels
+        // this is a monophonic synth
         1
     }
     fn sample_rate(&self) -> u32 {
         self.sample_rate as u32
     }
     fn current_frame_len(&self) -> Option<usize> {
+        // > Returns the number of samples before the current frame ends. None means “infinite” or
+        // > “until the sound ends”. Should never return 0 unless there’s no more data.
         None
     }
     fn total_duration(&self) -> Option<Duration> {
+        // > Returns the total duration of this source, if known.
+        // > None indicates at the same time “infinite” or “unknown”.
         None
+    }
+}
+
+// required for the `Source` trait
+impl Iterator for Oscillator {
+    type Item = f32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(self.get_sample())
     }
 }
 
