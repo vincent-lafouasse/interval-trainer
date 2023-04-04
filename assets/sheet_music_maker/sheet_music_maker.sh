a5d318e421656c91f1b823aa28111d91ac7b720d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-04-04 13:34:11 +0200

    refactor

--- assets/sheet_music_maker/sheet_music_maker.sh
+++ assets/sheet_music_maker/sheet_music_maker.sh
@@ -1,26 +1,52 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -o xtrace
 
+setup_globals() {
+	MAIN="main"
+
+	AUX_DIR="aux"
+	TARGET_DIR="target"
+
+	mkdir "${AUX_DIR}"
+	mkdir "${TARGET_DIR}"
+
+	TEMPLATE="template/template.ly"
+	LILY_FILE="${AUX_DIR}/${MAIN}.ly"
+	OUTPUT="${TARGET_DIR}/${MAIN}.pdf"
+}
+
+fill_template() {
+	TIME_SIG="2/4"
+	CLEF="treble"
+	NOTES="c'2"
+	export TIME_SIG CLEF NOTES
+
+	envsubst < "${TEMPLATE}" > "${LILY_FILE}"
+}
+
+clean() {
+	rm -rf "${AUX_DIR}"
+}
+
+mr_proper() {
+	clean
+	rm -rf "${TARGET_DIR}"
+}
+
 #################################################################################
+setup_globals
 
-MAIN="main"
-AUX_DIR="aux"
-TARGET_DIR="target"
-mkdir "${AUX_DIR}"
-mkdir "${TARGET_DIR}"
+if [[ "$1" == "--clean" ]]; then
+	clean
+	exit 0
+fi
+if [[ "$1" == "--mrproper" ]]; then
+	mr_proper
+	exit 0
+fi
 
-TEMPLATE="template/template.ly"
-LILY_FILE="${AUX_DIR}/${MAIN}.ly"
-OUTPUT="${TARGET_DIR}/${MAIN}.pdf"
-
-TIME_SIG="2/4"
-CLEF="treble"
-NOTES="c'2"
-export TIME_SIG CLEF NOTES
-
-envsubst < "${TEMPLATE}" > "${LILY_FILE}"
+fill_template
 lilypond --output="${TARGET_DIR}" "${LILY_FILE}"
 open "${OUTPUT}"
 
-rm -rf "${AUX_DIR}"
