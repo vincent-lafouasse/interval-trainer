4518df4c544d557c9c3740e70aa272129dba4cb5
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-16 17:09:01 +0200

    implement parse from string

--- src/notes.rs
+++ src/notes.rs
@@ -1,7 +1,7 @@
 pub struct Note {
     pub name: u8,
     pub alteration: i8,
-    pub octave: u8,
+    pub octave: i8,
 }
 
 impl Note {
@@ -22,18 +22,38 @@ impl Note {
             },
         };
 
-        let mut note = Note { name, alteration: 0, octave: 0 };
-
-        match chars.next() {
+        let mut alteration: i8 = match chars.next() {
             None => return Err("missing octave"),
             Some(c) => match c {
-                '0'..='9' => return Ok(Note { name, alteration: 0, octave: c as u8 - '0' as u8 }),
-                '#' => note.alteration = 1,
-                'b' => note.alteration = -1,
-                _ => return Err("error"),
+                '0'..='9' => return Ok(Note { name, alteration: 0, octave: c as i8 - '0' as i8 }),
+                '#' => 1,
+                'b' => -1,
+                _ => return Err("invalid alteration"),
+            },
+        };
+
+        alteration = match chars.next() {
+            None => return Err("missing octave"),
+            Some(c) => match c {
+                '0'..='9' => return Ok(Note { name, alteration, octave: c as i8 - '0' as i8 }),
+                '#' => match alteration {
+                    1 => 2,
+                    _ => return Err("invalid alterations"),
+                },
+                'b' => match alteration {
+                    -1 => -2,
+                    _ => return Err("invalid alterations"),
+                },
+                _ => return Err("invalid alteration"),
+            },
+        };
+
+        match chars.next() {
+            None => Err("missing octave"),
+            Some(c) => match c {
+                '0'..='9' => Ok(Note { name, alteration, octave: c as i8 - '0' as i8 }),
+                _ => Err("invalid note"),
             },
         }
-
-        Ok(note)
     }
 }
