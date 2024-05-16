51d5a4ee34d4e01ba16a2b6cdc482d7313ca1f3a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-16 16:39:33 +0200

    remove old code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
 
-mod frequencies;
-mod intervals;
 mod notes;
 mod synth;
 mod wavetables;
@@ -12,9 +10,6 @@ use rodio::{OutputStream, OutputStreamHandle, Sink};
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 
-use crate::frequencies::FREQUENCIES;
-use crate::intervals::{BaseInterval, Interval, Quality};
-use crate::notes::{Alteration, Note, NoteName, CHROMATIC_NOTES_PER_OCTAVE};
 use crate::synth::{Oscillator, Wavetable, WavetableSynth};
 
 const SAMPLE_RATE: usize = 44_100;
@@ -34,8 +29,8 @@ fn main() -> Result<()> {
     if synth {
         let synth = WavetableSynth::new(SINE, SAMPLE_RATE);
 
-        let f_a4: f32 = FREQUENCIES[4 * CHROMATIC_NOTES_PER_OCTAVE + 9];
-        let f_e5: f32 = FREQUENCIES[4 * CHROMATIC_NOTES_PER_OCTAVE + 9 + 7];
+        let f_a4: f32 = 440.0;
+        let f_e5: f32 = f_a4 * 1.5;
         let note_length_ms = 3000;
 
         synth.play(f_a4, note_length_ms, &stream_handle);
@@ -61,44 +56,4 @@ fn main() -> Result<()> {
 
 fn quiz() {
     println!("-----------------------------------------------------------");
-    let random_interval = Interval::get_random_diatonic();
-    let random_start_note = Note::get_random_biased();
-
-    println!(
-        "What note is a {} above {}?",
-        random_interval, random_start_note
-    );
-
-    let computed_end_note = random_interval.note_up_from(random_start_note);
-
-    loop {
-        let user_note = Note::get_from_user();
-
-        if user_note == computed_end_note {
-            println!("ding ding you win");
-            break;
-        }
-
-        let user_interval = Interval::between(random_start_note, user_note);
-        println!("[EXTREMELY LOUD INCORRECT BUZZER]");
-        println!("{random_start_note} to {user_note} is not a {random_interval}, that is a {user_interval}\n");
-    }
-}
-
-fn debug() {
-    println!("-----------------------------------------------------------");
-    let random_interval = Interval::get_random_diatonic();
-
-    println!("Here's a random interval :\n\t{}", random_interval);
-    println!("Its size is {} semitones\n", random_interval.size());
-
-    let c = Note { name: NoteName::C, alteration: Alteration::Natural };
-
-    let up_from_c: Note = random_interval.note_up_from(c);
-    println!("{} is a {} up from C\n", up_from_c, random_interval);
-
-    let note1 = Note::get_random_biased();
-    let note2 = Note::get_random_biased();
-    println!("Here are two random notes: {} and {}", note1, note2);
-    println!("Between them is a {}", Interval::between(note1, note2));
 }
