82e38e02b973787a9983d58df8c6acdafb035109
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-16 18:12:14 +0200

    implement display for simple notes

--- src/simple_note.rs
+++ src/simple_note.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub struct SimpleNote {
     data: u8,
 }
@@ -5,5 +7,27 @@ pub struct SimpleNote {
 impl SimpleNote {
     pub fn octave(&self) -> i8 {
         (self.data / 12) as i8 - 1
+    }
+}
+
+impl fmt::Display for SimpleNote {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let name = match self.data / 12 {
+            0 => "C",
+            1 => "C#/Db",
+            2 => "D",
+            3 => "Eb/D#",
+            4 => "E",
+            5 => "F",
+            6 => "F#/Gb",
+            7 => "G",
+            8 => "Ab/G#",
+            9 => "A",
+            10 => "Bb/A#",
+            11 => "B",
+            _ => "X",
+        };
+
+        write!(f, "{}{}", name, self.octave())
     }
 }
