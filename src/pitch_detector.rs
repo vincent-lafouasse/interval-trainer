10d085ea7a6033675d76c866b77a068699b03039
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-17 15:41:22 +0200

    sketch

--- src/pitch_detector.rs
+++ src/pitch_detector.rs
@@ -3,6 +3,8 @@ use std::sync::atomic::Ordering;
 use std::sync::Arc;
 use std::task::Context;
 
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+
 pub struct MyPitchDetector {
     config: MyPitchDetectorConfig,
     context: MyPitchDetectorContext,
@@ -43,6 +45,17 @@ pub struct MyPitchDetectorContext {
 
 impl MyPitchDetectorContext {
     pub fn setup(config: MyPitchDetectorConfig) -> Result<Self, &'static str> {
-        todo!()
+        let host: cpal::Host = cpal::default_host();
+        let input_device: cpal::Device = match host.default_input_device() {
+            Some(device) => device,
+            None => return Err("no input device available"),
+        };
+        let stream_config = cpal::StreamConfig {
+            channels: config.n_channels,
+            sample_rate: cpal::SampleRate(config.sample_rate.into()),
+            buffer_size: cpal::BufferSize::Default,
+        };
+
+        Ok(Self { host, input_device, stream_config })
     }
 }
