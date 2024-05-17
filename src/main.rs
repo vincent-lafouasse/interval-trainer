d0f6eade26603174d59495b522ab881f76bf0f63
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-17 20:43:13 +0200

    restructure

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ mod wavetables;
 use color_eyre::eyre::Result;
 use std::time::Duration;
 
+use eframe::egui;
+
 use crate::interval::{Direction, Interval};
 use crate::listen::listen_for_note;
 use crate::note_range::NoteRange;
@@ -22,8 +24,40 @@ use crate::synth::play_notes;
 
 const SAMPLE_RATE: u16 = 44_100;
 
+#[derive(Default)]
+struct IntervalTrainer {}
+
+impl eframe::App for IntervalTrainer {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            egui::ScrollArea::both().show(ui, |ui| {
+                ui.add(
+                    egui::Image::new("https://picsum.photos/seed/1.759706314/1024").rounding(10.0),
+                );
+
+                ui.image(egui::include_image!("../assets/svg/.svg"));
+            });
+        });
+    }
+}
+
 fn main() -> Result<()> {
     color_eyre::install()?;
+
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([600.0, 800.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Interval Trainer",
+        options,
+        Box::new(|cc| {
+            // This gives us image support:
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+            Box::<IntervalTrainer>::default()
+        }),
+    );
 
     let range = NoteRange::tenor_voice();
     let (reference_note, mystery_note) = choose_notes(&range);
