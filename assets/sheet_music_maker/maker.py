5ee9422bfd690935e0f52222d87aba1e89176794
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-17 17:48:01 +0200

    sketch Alteration class

--- assets/sheet_music_maker/maker.py
+++ assets/sheet_music_maker/maker.py
@@ -10,11 +10,32 @@ class Note:
     def __init__(self, name, alteration, octave):
         assert name in "ABCDEFG"
         assert octave in range(9)
-        print("new note {} {} {}", name, alteration, octave)
+        print(f"new note {name}{alteration.str_repr()}{octave}")
 
+class Alteration:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def lilypond_repr(self):
+        pass
+
+    @staticmethod
+    def str_repr(self):
+        pass
+
+class NoAlteration (Alteration):
+    def __init__(self):
+        pass
+
+    def lilypond_repr(self):
+        return ""
+
+    def str_repr(self):
+        return ""
 
 def main():
-    note = Note("C", "#", 3)
+    note = Note("C", NoAlteration(), 3)
 
 
 if __name__ == "__main__":
