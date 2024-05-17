4692afffc3a3c07c493b363d55e55f0fca54b5cd
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-17 23:25:26 +0200

    always show accidentals

--- tools/sheet_music_maker/lilypond_file_generator/lilypond_file_generator.py
+++ tools/sheet_music_maker/lilypond_file_generator/lilypond_file_generator.py
@@ -23,7 +23,7 @@ class LilypondFile:
             output.write("	\\time 100/2 % no bar lines (probably) \n")
             output.write(f"	\\clef {self.clef.get()} \n")
             output.write("	\\key c \\major \n")
-            output.write(f"	| {self.note.ly_repr()}1 {self.note.ly_repr()}1 | \n")
+            output.write(f"	| {self.note.ly_repr()}!1 {self.note.ly_repr()}!1 | \n")
             output.write("} \n")
 
 
