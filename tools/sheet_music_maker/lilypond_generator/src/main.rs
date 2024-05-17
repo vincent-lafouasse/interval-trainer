8c2c4a36797c04c3f0d349e7ad687d9e51bdbe0b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-18 01:16:11 +0200

    sketch

--- tools/sheet_music_maker/lilypond_generator/src/main.rs
+++ tools/sheet_music_maker/lilypond_generator/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
     println!("Hello, world!");
 }
+
+trait LilypondThing {
+    fn lily_repr(&self) -> &str;
+}
+
+type NoteName = char;
+impl LilypondThing for NoteName {
+    fn lily_repr(&self) -> &str {
+        match *self {
+            'C' => "c",
+            'D' => "d",
+            'E' => "e",
+            'F' => "f",
+            'G' => "g",
+            'A' => "a",
+            'B' => "b",
+            _ => panic!("invalid notename: {}", *self),
+        }
+    }
+}
