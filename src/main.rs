3a848ffaf22a3647ef1a293fcb76853f34a389fb
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-20 16:04:14 +0200

    comment

--- src/main.rs
+++ src/main.rs
@@ -22,33 +22,6 @@ use std::time::Duration;
 
 use crate::interval_trainer::IntervalTrainer;
 
-/*
-* backend usage:
-*
-    let range = NoteRange::tenor_voice();
-    let (reference_note, mystery_note) = choose_notes(&range);
-
-    println!("This is {}", reference_note);
-    play_notes(
-        reference_note,
-        mystery_note,
-        Duration::from_millis(1000),
-        SAMPLE_RATE,
-    );
-
-    match listen_for_note(
-        mystery_note.to_simple(),
-        Duration::from_millis(1500),
-        SAMPLE_RATE,
-    ) {
-        Some(cent_deviation) => println!(
-            "you got it ! it was {}\nyou got it within a {} cent deviation",
-            mystery_note, cent_deviation
-        ),
-        None => println!("womp womp it was {}", mystery_note),
-    }
-*/
-
 const SAMPLE_RATE: u16 = 44_100;
 
 fn main() -> Result<()> {
@@ -77,3 +50,30 @@ fn main() -> Result<()> {
 fn print_type_of<T>(_: &T) {
     println!("{}", std::any::type_name::<T>())
 }
+
+/*
+* backend usage:
+*
+    let range = NoteRange::tenor_voice();
+    let (reference_note, mystery_note) = choose_notes(&range);
+
+    println!("This is {}", reference_note);
+    play_notes(
+        reference_note,
+        mystery_note,
+        Duration::from_millis(1000),
+        SAMPLE_RATE,
+    );
+
+    match listen_for_note(
+        mystery_note.to_simple(),
+        Duration::from_millis(1500),
+        SAMPLE_RATE,
+    ) {
+        Some(cent_deviation) => println!(
+            "you got it ! it was {}\nyou got it within a {} cent deviation",
+            mystery_note, cent_deviation
+        ),
+        None => println!("womp womp it was {}", mystery_note),
+    }
+*/
