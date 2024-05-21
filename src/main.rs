44712968e9a56734372e2f51e8f0bd6d378e144f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-21 18:11:10 +0200

    sketch

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,48 @@ mod simple_note;
 mod synth;
 mod wavetables;
 
-use color_eyre::eyre::Result;
+use std::path::Path;
+
+use sdl2::event::Event;
+use sdl2::image::{InitFlag, LoadTexture};
+use sdl2::keyboard::Keycode;
 
 
-fn main() -> Result<()> {
-    color_eyre::install()?;
+fn main() -> Result<(), String> {
+    let png_dir = Path::new("src/assets/png");
+    let cool_png = png_dir.join("treble_staff.png");
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let _image_context = sdl2::image::init(InitFlag::PNG)?;
+    let window = video_subsystem
+        .window("rust-sdl2 demo: Video", 800, 600)
+        .position_centered()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut canvas = window
+        .into_canvas()
+        .software()
+        .build()
+        .map_err(|e| e.to_string())?;
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture(cool_png)?;
+
+    canvas.copy(&texture, None, None)?;
+    canvas.present();
+
+    'mainloop: loop {
+        for event in sdl_context.event_pump()?.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                    | Event::KeyDown {
+                    keycode: Option::Some(Keycode::Escape),
+                    ..
+                } => break 'mainloop,
+                _ => {}
+            }
+        }
+    }
 
     Ok(())
 }
