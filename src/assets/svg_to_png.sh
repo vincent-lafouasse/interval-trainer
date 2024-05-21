669393ad92ad9ed8c1df2cff61af695e67ba53b1
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-21 17:55:04 +0200

    convert svg to png using inkscape

--- src/assets/svg_to_png.sh
+++ src/assets/svg_to_png.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
-set -o xtrace
 
-echo $(basename $1)
+die() {
+	>&2 echo "$1"
+	exit 1
+}
+
+main() {
+	if [ $# -ne 1 ]; then
+		die "Usage: $0 file.svg"
+	fi
+
+	name="$(basename "$1")"
+
+	if [[ $name != *svg ]] ;
+	then
+		die "Usage: $0 file.svg"
+	fi
+
+	base_name="${name/\.svg/}"    
+
+	inkscape -w 500 "$base_name".svg -o "$base_name".png
+}
+
+main "$@"
