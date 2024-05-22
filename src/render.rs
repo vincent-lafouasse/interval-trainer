c2b86c6458fb99ab0391acf3895891846b8a8d55
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-22 13:52:21 +0200

    implement Position::shift

--- src/render.rs
+++ src/render.rs
@@ -6,6 +6,8 @@ use sdl2::{
     render::RenderTarget,
 };
 
+const STAFF_SPACING: u32 = 40;
+
 pub fn render_staff<T: RenderTarget>(
     staff: &sdl2::render::Texture,
     canvas: &mut sdl2::render::Canvas<T>,
@@ -19,7 +21,7 @@ pub fn render_note<T: RenderTarget>(
     note_head: &sdl2::render::Texture,
     canvas: &mut sdl2::render::Canvas<T>,
 ) -> Result<(), String> {
-    let pos = Position { x: 420, y: 69 };
+    let pos = Position { x: 420, y: 150 };
 
     render_at(pos, note_head, canvas)
 }
@@ -38,6 +40,16 @@ pub fn render_at<T: RenderTarget>(
 pub struct Position {
     x: i32,
     y: i32,
+}
+
+impl Position {
+    fn shift_x(&self, amount: i32) -> Self {
+        Self { x: self.x + amount, y: self.y }
+    }
+
+    fn shift_y(&self, amount: i32) -> Self {
+        Self { x: self.x, y: self.y + amount }
+    }
 }
 
 pub struct Dimension {
